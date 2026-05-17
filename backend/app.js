11cef70ad1f8963a96a768c8faf4e114b4f2abfb
abhinav21s <abhinav.sharma01030@gmail.com>
2026-05-17 06:36:03 +0000

    Added a health route

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,18 @@ const connectdb=require('./db.js')
 const route=require("./route.js")
 const cors = require("cors")
 const app = express();
+
 app.use(express.json())
 app.use(cors())
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Server is running"
+  })
+})
 app.use("/cypher",route)
 connectdb();
+
 
 app.listen(3000,()=>{
     console.log("Server started")
